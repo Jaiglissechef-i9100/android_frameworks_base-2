3ebd9510a7cc6d52bd108053168c8a54049c82cd
Kevin Chyn <kchyn@google.com>
2018-03-20 14:42:39 -0700

    Fingerprint navigation settings is a secure setting
    Fixes: 76025501
    
    Test: turning on/off the navigation setting actually turns on/off
          the ability to swipe the notification panel
    
    Change-Id: I465fcebfc1e248bd37a609f404462ae8a45cf31b
    Merged-In: I465fcebfc1e248bd37a609f404462ae8a45cf31b
    Signed-Off-By: Siarhei Vishniakou <svv@google.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1223,7 +1223,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.ANBI_ENABLED), false, this,
                     UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.Global.getUriFor(
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.SYSTEM_NAVIGATION_KEYS_ENABLED), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
